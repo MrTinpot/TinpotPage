08fe25259a58f31a03f2ab6cfc637b0f2e3317f8
MrTinpot <147400116+MrTinpot@users.noreply.github.com>
2023-10-16 19:57:15 +0300

    WEBSITE

---
+++ JS-BASICS/javas.js
@@ -0,0 +1,21 @@
+//print
+console.log("Hello World TINPOT IS HERE");
+
+//Değişken
+let tinpot = "schzio";
+let r2da = "alive again";
+console.log(tinpot," ",r2da)
+
+//Değişmeyen Değiken
+const faiz = 0.3;
+console.log(faiz)
+
+let name = "Ege"; //string
+let age = 15; //number integer ve float yok
+let isVerified = false; //boolean
+let password = undefined; //belirsiz
+let RGBselecet = null; //Object
+
+
+
+
